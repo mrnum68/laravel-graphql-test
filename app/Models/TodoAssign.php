b6fd089a3af9a57495d97c585b6d82db7ea13d8b
nam.tp <nam.tp@neo-lab.vn>
2022-08-09 14:04:01 +0700

    add relation

--- app/Models/TodoAssign.php
+++ app/Models/TodoAssign.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class TodoAssign extends Model
 {
     use HasFactory;
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function todo() {
+        return $this->belongsTo(Todo::class, 'todo_id');
+    }
 }
